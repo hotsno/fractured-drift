0b03e200f7d8e415a125f7a2ca9493a3937b06b5
hotsno <71658949+hotsno@users.noreply.github.com>
2024-08-17 23:56:58 -0700

    Remove sprint, make jump higher

--- Common/Player/Player.gd
+++ Common/Player/Player.gd
@@ -3,16 +3,12 @@ extends CharacterBody3D
 var speed
 var speed_multiplier = 1.0
 const WALK_SPEED = 5.0
-const SPRINT_SPEED = 8.0
-const JUMP_VELOCITY = 4.8
+const JUMP_VELOCITY = 6.0
 const SENSITIVITY = 0.004
 
 const BOB_FREQ = 2.4
 const BOB_AMP = 0.08
 var t_bob = 0.0
-
-const BASE_FOV = 75.0
-const FOV_CHANGE = 1.5
 
 var gravity = 15
 
@@ -58,10 +54,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	if Input.is_action_pressed("sprint"):
-		speed = SPRINT_SPEED * speed_multiplier
-	else:
-		speed = WALK_SPEED * speed_multiplier
+	speed = WALK_SPEED * speed_multiplier
 
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
@@ -78,10 +71,6 @@ func _physics_process(delta):
 
 	t_bob += delta * velocity.length() * float(is_on_floor())
 	camera.transform.origin = _headbob(t_bob)
-
-	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
-	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
-	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
 
 	_run_pickup_object_logic()
 	move_and_slide()
