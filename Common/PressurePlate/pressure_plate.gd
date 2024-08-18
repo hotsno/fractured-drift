f3d2c13d38abf476b05e0fa6921230d1cbdf84a1
hotsno <71658949+hotsno@users.noreply.github.com>
2024-08-18 00:51:49 -0700

    Add portal to level 1

--- Common/PressurePlate/pressure_plate.gd
+++ Common/PressurePlate/pressure_plate.gd
@@ -6,6 +6,5 @@ func _ready():
 	connect("body_entered", _on_body_entered)
 
 func _on_body_entered(body):
-	if body:
-		if body.name == "Player":
+	if body and body.name == "Player":
 			stepped_on.emit()
