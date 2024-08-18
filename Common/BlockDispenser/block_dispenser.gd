e5decbc096b3840884aac00930e0ae0a024ca042
hotsno <71658949+hotsno@users.noreply.github.com>
2024-08-17 23:53:08 -0700

    Add block dispenser to level 1

--- Common/BlockDispenser/block_dispenser.gd
+++ Common/BlockDispenser/block_dispenser.gd
@@ -6,15 +6,11 @@ signal stepped_on
 @export var spawn_location = Vector3i(1, 10, 1)
 var block_instance: Node
 
-func spawn_block():
+func _on_pressure_plate_stepped_on():
+	stepped_on.emit()
+	if block_instance:
+		block_instance.queue_free()
 	var new_block_instance = block_scene.instantiate()
 	new_block_instance.position = spawn_location
 	add_child(new_block_instance)
 	self.block_instance = new_block_instance
-
-func _on_pressure_plate_stepped_on():
-	stepped_on.emit()
-	if not block_instance:
-		spawn_block()
-	else:
-		block_instance.position = spawn_location
