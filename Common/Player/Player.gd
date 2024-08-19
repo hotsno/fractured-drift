f255a205d4598c2396651f4ae178828d864c2ce2
hotsno <71658949+hotsno@users.noreply.github.com>
2024-08-19 15:26:12 -0700

    Fix player direction

--- Common/Player/Player.gd
+++ Common/Player/Player.gd
@@ -60,7 +60,7 @@ func _physics_process(delta):
 	speed = WALK_SPEED * speed_multiplier
 
 	var input_dir = Input.get_vector("left", "right", "up", "down")
-	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (global_transform.basis * head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if is_on_floor():
 		if direction:
 			velocity.x = direction.x * speed
